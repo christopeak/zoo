07c8ae2e3633e75a8c1edbceab0c649451245513
Chris Peak <cpeak@psrc.org>
2015-04-20 12:51:35 +0000

    added zebra and tiger

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150420125056) do
+
+  create_table "tigers", force: :cascade do |t|
+    t.integer  "appetite"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "zebras", force: :cascade do |t|
+    t.integer  "weight"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
